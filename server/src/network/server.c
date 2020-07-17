3bba48ecee83655a8c12d9f4f7415206dc50ded1
Ben <ben@wolkenwelten.net>
2020-07-18 00:57:33 +0200

    Tried rewriting serverParsePacket to not copy around all the other packet bytes after parsing a single one of them, for now break the server completely

--- server/src/network/server.c
+++ server/src/network/server.c
@@ -289,33 +289,49 @@ void serverParsePacketHuge(int c, packetHuge *p){
 }
 
 void serverParsePacket(int i){
+	unsigned int off = 0;
+	
+	if(clients[i].recvBufLen < 16){return;}
 	for(int max=16;max > 0;--max){
-		if(clients[i].recvBufLen < 16){ return; }
-		unsigned int pLen = packetLen(clients[i].recvBuf);
-		if(pLen <= 0){ return; }
-		if(pLen > clients[i].recvBufLen){ return; }
+		if((clients[i].recvBufLen - off) < 16)  { break; }
+		unsigned int pLen = packetLen(clients[i].recvBuf + off);
+		if(pLen > (clients[i].recvBufLen - off)){ break; }
 
 		switch(pLen){
 			case sizeof(packetSmall):
-				serverParsePacketSmall (i, (packetSmall *)  clients[i].recvBuf);
+				serverParsePacketSmall (i, (packetSmall *)  (clients[i].recvBuf+off));
 			break;
 			case sizeof(packetMedium):
-				serverParsePacketMedium(i, (packetMedium *) clients[i].recvBuf);
+				serverParsePacketMedium(i, (packetMedium *) (clients[i].recvBuf+off));
 			break;
 			case sizeof(packetLarge):
-				serverParsePacketLarge (i, (packetLarge *)  clients[i].recvBuf);
+				serverParsePacketLarge (i, (packetLarge *)  (clients[i].recvBuf+off));
 			break;
 			case sizeof(packetHuge):
-				serverParsePacketHuge  (i, (packetHuge *)   clients[i].recvBuf);
+				serverParsePacketHuge  (i, (packetHuge *)   (clients[i].recvBuf+off));
+			break;
+			default:
+				fprintf(stderr,"Unknown packet size: %i \n",pLen);
+				serverKill(i);
+				break;
 			break;
 		}
-
-		if(clients[i].recvBufLen != pLen){
-			for(unsigned int ii=0;ii < (clients[i].recvBufLen - pLen);++ii){
-				clients[i].recvBuf[ii] = clients[i].recvBuf[ii+pLen];
-			}
+		off += pLen;
+	}
+	
+	if(off > clients[i].recvBufLen){
+		fprintf(stderr,"Error, offset bigger than buffer length, this should never happen.\n");
+		serverKill(i);
+		return;
+	}else if(off == clients[i].recvBufLen){
+		fprintf(stderr,"ZERO\n");
+		clients[i].recvBufLen = 0;
+	}else{
+		fprintf(stderr,"MOVE off:%i len:%i\n",off,clients[i].recvBufLen);
+		for(unsigned int ii=0;ii < (clients[i].recvBufLen - off);++ii){
+			clients[i].recvBuf[ii] = clients[i].recvBuf[ii+off];
 		}
-		clients[i].recvBufLen -= pLen;
+		clients[i].recvBufLen -= off;
 	}
 }
 
@@ -537,6 +553,7 @@ void dumpQueue(int c){
 
 void addQueuedChunks(int c){
 	while(clients[c].sendBufLen < ((1<<22)-8192)){
+		if((c < 0) || (c >= clientCount)){return;}
 		if(clients[c].chnkReqQueueLen == 0){
 			if(clients[c].chngReqQueueLen == 0){
 				return;
@@ -607,6 +624,7 @@ int addWSMessagePrefix(uint8_t *d, int len, int maxlen){
 void sendToClient(int c,void *data,int len){
 	int ret;
 	int tlen = len;
+	int tries = 10;
 	if(c < 0){return;}
 	if(c >= clientCount){return;}
 	if(clients[c].flags&1){
@@ -618,6 +636,10 @@ void sendToClient(int c,void *data,int len){
 		if(ret == 1){
 			usleep(10);
 		}else if(ret == 2){
+			return;
+		}
+		if(--tries < 0){
+			serverKill(c);
 			return;
 		}
 	}
