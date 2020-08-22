f0c1ef3b80ff41c26eb63e2dc2da99860d6c175c
Ben <ben@wolkenwelten.net>
2020-08-22 22:08:06 +0200

    Some item sprite work and reenabled the check for full hp before eating a pear

--- common/src/mods/items/pear.c
+++ common/src/mods/items/pear.c
@@ -10,7 +10,7 @@ bool pearSecondaryAction(item *cItem,character *cChar, int to){
 	(void)cItem;
 
 	if(to < 0){return false;}
-	//if(characterGetHP(cChar) >= characterGetMaxHP(cChar)){return false;}
+	if(characterGetHP(cChar) >= characterGetMaxHP(cChar)){return false;}
 	if(itemDecStack(cItem,1)){
 		characterHP(cChar,8);
 		characterAddCooldown(cChar,200);
