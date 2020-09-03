57635386711d665ec8ea8b66647b0fa6b78bcb80
Ben <ben@wolkenwelten.net>
2020-09-03 20:54:21 +0200

    Improved look of beam/explosion partile effects

--- client/src/gfx/effects.c
+++ client/src/gfx/effects.c
@@ -13,25 +13,25 @@
 void fxExplosionBomb(float x,float y,float z,float pw){
 	sfxPlay(sfxBomb,1.f);
 
-	for(int i=0;i<512*pw;i++){
+	for(int i=0;i<4096*pw;i++){
 		float vx = (rngValf()-0.5f)/6.f*pw*2;
 		float vy = (rngValf()-0.5f)/6.f*pw*2;
 		float vz = (rngValf()-0.5f)/6.f*pw*2;
 		newParticle(x,y,z,vx,vy,vz,vx/-64.f,vy/-64.f,vz/-64.f,0xFF44AAFF,64);
 	}
-	for(int i=0;i<512*pw;i++){
+	for(int i=0;i<4096*pw;i++){
 		float vx = (rngValf()-0.5f)/10.f*pw*2;
 		float vy = (rngValf()-0.5f)/10.f*pw*2;
 		float vz = (rngValf()-0.5f)/10.f*pw*2;
 		newParticle(x,y,z,vx,vy,vz,vx/-78.f,vy/-78.f,vz/-78.f,0xFF0099FF,78);
 	}
-	for(int i=0;i<256*pw;i++){
+	for(int i=0;i<2048*pw;i++){
 		float vx = (rngValf()-0.5f)/16.f*pw*2;
 		float vy = (rngValf()-0.5f)/16.f*pw*2;
 		float vz = (rngValf()-0.5f)/16.f*pw*2;
 		newParticle(x,y,z,vx,vy,vz,vx/-96.f,vy/-96.f,vz/-96.f,0xFF0066CC,96);
 	}
-	for(int i=0;i<256*pw;i++){
+	for(int i=0;i<2048*pw;i++){
 		float vx = (rngValf()-0.5f)/32.f*pw*2;
 		float vy = (rngValf()-0.5f)/32.f*pw*2;
 		float vz = (rngValf()-0.5f)/32.f*pw*2;
@@ -65,17 +65,17 @@ void fxGrenadeTrail(float x,float y,float z,float pw){
 }
 
 void fxExplosionBlaster(float x,float y,float z,float pw){
-	for(int i=0;i<128;i++){
+	for(int i=0;i<512;i++){
 		float vx = (rngValf()-0.5f)/8.f*pw;
 		float vy = (rngValf()-0.5f)/8.f*pw;
 		float vz = (rngValf()-0.5f)/8.f*pw;
-		newParticle(x,y,z,vx,vy,vz,vx/-64.f,vy/-64.f,vz/-64.f,0xFF3A1F90,64);
+		newParticle(x,y,z,vx,vy,vz,vx/-64.f,vy/-64.f,vz/-64.f,0xFF964AC0,96);
 	}
-	for(int i=0;i<128;i++){
+	for(int i=0;i<512;i++){
 		float vx = (rngValf()-0.5f)/12.f*pw;
 		float vy = (rngValf()-0.5f)/12.f*pw;
 		float vz = (rngValf()-0.5f)/12.f*pw;
-		newParticle(x,y,z,vx,vy,vz,vx/-96.f,vy/-96.f,vz/-96.f,0xFF7730A0,128);
+		newParticle(x,y,z,vx,vy,vz,vx/-96.f,vy/-96.f,vz/-96.f,0xFF7730A0,78);
 	}
 }
 void fxBeamBlaster(float x1,float y1,float z1,float x2,float y2,float z2, float beamSize, float damageMultiplier, float recoilMultiplier, int hitsLeft, int originatingCharacter){
@@ -93,9 +93,9 @@ void fxBeamBlaster(float x1,float y1,float z1,float x2,float y2,float z2, float 
 	if(fabsf(dy) > dm){dm = fabsf(dy);}
 	if(fabsf(dz) > dm){dm = fabsf(dz);}
 
-	float vx = (dx / dm)/(4.f*beamSize);
-	float vy = (dy / dm)/(4.f*beamSize);
-	float vz = (dz / dm)/(4.f*beamSize);
+	float vx = (dx / dm)/(24.f*beamSize);
+	float vy = (dy / dm)/(24.f*beamSize);
+	float vz = (dz / dm)/(24.f*beamSize);
 
 	if(fabsf(vx) > fabsf(vy)){
 		if(fabsf(vx) > fabsf(vz)){
@@ -112,14 +112,15 @@ void fxBeamBlaster(float x1,float y1,float z1,float x2,float y2,float z2, float 
 	}
 	uint32_t pac = 0xFF0000FF | ((0x50 + rngValM(0x30)) << 16) | ((0x30 + rngValM(0x30)) << 8);
 	uint32_t pbc = pac + 0x00202000;
+	int plifetime = MIN(96,MAX(16,48 * damageMultiplier));
 
 	sfxPlay(sfxPhaser,MAX(0.5f,damageMultiplier));
 	for(;steps > 0;steps--){
-		float pvx = (rngValf()-0.5f)/8.f*beamSize;
-		float pvy = (rngValf()-0.5f)/8.f*beamSize;
-		float pvz = (rngValf()-0.5f)/8.f*beamSize;
-		newParticle(cx+pvx,cy+pvy,cz+pvz,pvx/4.f,pvy/4.f,pvz/4.f,-pvx/192.f,-pvy/192.f,-pvz/192.f,pac,64);
-		newParticle(cx+pvx,cy+pvy,cz+pvz,pvx/6.f,pvy/6.f,pvz/6.f,-pvx/256.f,-pvy/256.f,-pvz/256.f,pbc,96);
+		float pvx = (rngValf()-0.5f)/24.f*beamSize;
+		float pvy = (rngValf()-0.5f)/24.f*beamSize;
+		float pvz = (rngValf()-0.5f)/24.f*beamSize;
+		newParticle(cx+pvx,cy+pvy,cz+pvz,pvx/4.f,pvy/4.f,pvz/4.f,-pvx/192.f,-pvy/192.f,-pvz/192.f,pac,plifetime*3);
+		newParticle(cx+pvx,cy+pvy,cz+pvz,pvx/6.f,pvy/6.f,pvz/6.f,-pvx/256.f,-pvy/256.f,-pvz/256.f,pbc,plifetime*4);
 		cx += vx;
 		cy += vy;
 		cz += vz;
