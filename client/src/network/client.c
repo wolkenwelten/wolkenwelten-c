06d2c085a4c24728bf6f7cff0f5f380e416613b4
Ben <ben@wolkenwelten.net>
2020-10-04 02:35:14 +0200

    Removed playerName debug printf

--- client/src/network/client.c
+++ client/src/network/client.c
@@ -164,7 +164,6 @@ void clientParsePacket(const packet *p){
 			characterMoveDelta(player,p);
 			break;
 		case 14: // characterName
-			fprintf(stderr,"characterName[%i]: %s\n",pLen,&p->val.c[2]);
 			characterSetName(p);
 			break;
 		case 15: // playerPos
