17242b21e269de60c1fc3fb41bf5f3e91b9d2b9d
Ben <ben@wolkenwelten.net>
2020-10-18 19:02:03 +0200

    Fixed a typo leading to too many chunks being generated in a single frame, blocking everything for a couple seconds in the worst case

--- client/src/voxel/chunk.c
+++ client/src/voxel/chunk.c
@@ -24,7 +24,7 @@ uint chunkCount     = 0;
 uint chunksGeneratedThisFrame = 0;
 chunk *chunkFirstFree = NULL;
 
-#define MAX_CHUNKS_GEN_PER_FRAME 488
+#define MAX_CHUNKS_GEN_PER_FRAME 32
 
 #ifdef __EMSCRIPTEN__
 	chunk chunkList[1<<16];
