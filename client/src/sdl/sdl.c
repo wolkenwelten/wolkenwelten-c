b181281d41c3702b3819942b29a1d4b759b52c42
Ben <ben@wolkenwelten.net>
2020-12-10 03:30:32 +0100

    Switched to Desktop OGL on linux

--- client/src/sdl/sdl.c
+++ client/src/sdl/sdl.c
@@ -75,11 +75,7 @@ void initSDL(){
 		exit(1);
 	}
 
-	#ifdef __LINUX__
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
-	#elif defined __EMSCRIPTEN__
+	#if defined __EMSCRIPTEN__
 		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
 		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
 	#else
