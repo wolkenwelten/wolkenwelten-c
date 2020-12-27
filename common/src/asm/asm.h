617c6b7dcc2886638916cc90dea8e1bb55e2918b
Ben <ben@wolkenwelten.net>
2020-12-27 03:38:55 +0100

    Fixed macos

--- common/src/asm/asm.h
+++ common/src/asm/asm.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#ifndef __APPLE__
 
 #ifdef __aarch64__
 	#define WW_ASM_PARTICLE_POS_UPDATE
@@ -7,4 +8,6 @@
 
 #ifdef __x86_64__
 	#define WW_ASM_PARTICLE_POS_UPDATE
+#endif
+
 #endif
