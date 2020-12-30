61da6065e351f60d343b89bf218a84167f6ea922
Ben <ben@wolkenwelten.net>
2020-12-30 05:21:55 +0100

    Grassgrowth

--- server/src/game/landscape.c
+++ server/src/game/landscape.c
@@ -21,16 +21,30 @@ static void landscapeUpdateChunk(chunk *c){
 		}
 	}
 
-	for(uint x=0;x<16;x++){
-	for(uint y=0;y<16;y++){
-	for(uint z=0;z<16;z++){
+	for(uint x=0; x < 16; x++){
+	for(uint y=0; y < 16; y++){
+	for(uint z=0; z < 16; z++){
 		const u8 b = c->data[x][y][z];
 		water *w = waters[x][y][z];
 		switch(b){
 		default:
 			break;
+		case I_Dirt:
+			if((w != NULL) && (w->amount > 128) && (y < 15) && (c->data[x][y+1][z] == 0)){
+				uint i = 20;
+				u8 cx = (x-1) + (rngValA(1)<<1);
+				u8 cy = (y-1) + (rngValA(1)<<1);
+				u8 cz = (z-1) + (rngValA(1)<<1);
+				if(c->data[cx][cy][cz] == I_Dry_Grass){ i = 16; }
+				if(c->data[cx][cy][cz] == I_Grass)    { i = 12; }
+				if(rngValA((1 << i)-1) == 0){
+					worldSetB(c->x+x,c->y+y,c->z+z,I_Dry_Grass);
+					w->amount -= 128;
+				}
+			}
+			break;
 		case I_Grass:
-			if((w == NULL) && (rngValA((1<<16)-1) == 0)){
+			if((w == NULL) && (rngValA((1<<14)-1) == 0)){
 				worldSetB(c->x+x,c->y+y,c->z+z,I_Dry_Grass);
 			}
 			break;
