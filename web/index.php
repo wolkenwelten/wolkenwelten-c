df870fdcf6f46844aa29a2ecd414ee798fde0fe0
Ben (X220/Arch) <ben@wolkenwelten.net>
2021-02-12 16:18:19 +0100

    Rewrote some Web texts

--- web/index.php
+++ web/index.php
@@ -33,10 +33,20 @@ foreach(array_keys($platforms) as $plat){
 <body bgcolor="#333333" text="#cccccc"  link="#cc3366" vlink="#cc3366" alink="#cc3366">
 	<h1>WolkenWelten - Alpha</h1>
 	<hr/>
-	<p>Greetings <b><?php echo hyper("Cybersurfer"); ?></b>, you have reached my little corner of the WWW offering a sneak peek of a videogame I have been developing in my spare time.</p>
-	<p>You can watch me work on this Game on my <a href="https://www.twitch.tv/melchizedek6809">Twitch channel</a>, I try to do more visually exciting features on stream but it is still just me coding away, so be prepared for that.</p>
-	<p>To find other People to play with and/or smalltalk about LISP be sure to Join our <a href="https://discord.gg/vPZffVS">Discord</a>.</p>
-	<h2>Download a stable release:</h2>
+	<p><b><?php echo hyper("Greetings Cybersurfer"); ?></b>, here you may find the newest release builds of the Free Software, LISP-Powered Voxel Sandbox called WolkenWelten (German for CloudWorlds).</p>
+	<p>Everything is licensed under the AGPLv3 and you can follow and hopefully join the development effort over on <a href="https://sr.ht/~melchizedek6809/WolkenWelten/">SourceHut</a> or on <a href="https://github.com/Melchizedek6809/WolkenWelten">GitHub</a>.</p>
+	<p>As for myself, my name is Ben, the lead dev of this project and you can watch me work on this Game on my <a href="https://www.twitch.tv/melchizedek6809">Twitch channel</a>, I try to do more visually exciting features on stream but it is still just me coding away, so be prepared for that.</p>
+	<p>To find other People to play with and/or smalltalk about LISP be sure to Join our <a href="https://discord.gg/vPZffVS">Discord</a> or <a href="https://matrix.to/#/!RKZztYPGhtlgALDvMS:matrix.org?via=matrix.org">Matrix</a> communities.</p>
+
+	<h2>Something to look at</h2>
+	<div align=center class=pics>
+		<a href="img/1.jpg"><img src="<?php echo baseSFS('img/1.jpg'); ?>" width=1920 height=1080/></a>
+		<a href="img/2.jpg"><img src="<?php echo baseSFS('img/2.jpg'); ?>" width=1920 height=1080/></a>
+		<a href="img/6.jpg"><img src="<?php echo baseSFS('img/6.jpg'); ?>" width=1920 height=1080/></a>
+	</div>
+	<hr/>
+
+	<h2>Download a Stable release:</h2>
 	<hr/>
 	<div class=nativedl align=center>
 		<?php echoBuilds($stable); ?>
@@ -44,7 +54,7 @@ foreach(array_keys($platforms) as $plat){
 	</div>
 	<br/>
 	<br/>
-	<h2>Or, try the latest and greatest experimental build:</h2>
+	<h2>Try the freshest experimental builds:</h2>
 	<hr/>
 	<div class=nativedl align=center>
 		<?php echoBuilds($experimental); ?>
@@ -52,27 +62,36 @@ foreach(array_keys($platforms) as $plat){
 
 	<hr/>
 	<br/>
-	<h2>Or play directly in your Browser using WebAssembly</h2>
+	<h2>Or play directly in you Browser, thanks to the magic that is Emscripten</h2>
 	<hr/>
 	<div class=wasmdl align=center>
 		<a href="releases/wasm/index.html?worldSeed=6809&savegame=Test" class=button><span class=buttonlabel>Start WASM version</span><span class="buttonicon icon-wasm"></span></a><br/>
-		<b>WARNING: Still quite experimental!.</b><br/>
+		<b>WARNING: Still quite experimental!</b><br/>
 		<i>Tested in latest Chrome/Firefox</i>
 	</div>
 	<hr/>
 	<br/>
 
-	<h2>Have some Screenshots</h2>
+	<h2>Some more pictures</h2>
 	<div align=center class=pics>
-		<a href="img/1.jpg"><img src="<?php echo baseSFS('img/1.jpg'); ?>" width=1920 height=1080/></a>
-		<a href="img/2.jpg"><img src="<?php echo baseSFS('img/2.jpg'); ?>" width=1920 height=1080/></a>
 		<a href="img/3.jpg"><img src="<?php echo baseSFS('img/3.jpg'); ?>" width=1920 height=1080/></a>
-		<a href="img/4.jpg"><img src="<?php echo baseSFS('img/4.jpg'); ?>" width=1920 height=1080/></a>
 		<a href="img/5.jpg"><img src="<?php echo baseSFS('img/5.jpg'); ?>" width=1920 height=1080/></a>
-		<a href="img/6.jpg"><img src="<?php echo baseSFS('img/6.jpg'); ?>" width=1920 height=1080/></a>
+		<a href="img/4.jpg"><img src="<?php echo baseSFS('img/4.jpg'); ?>" width=1920 height=1080/></a>
 	</div>
 	<hr/>
-	<h3>My priorities for this game</h3>
+	<h3>Cool things that are already true</h3>
+	<ul>
+	<li>mostly written in C99</li>
+	<li>runs at (close to) 60FPS on an unmodified Raspberry PI 400</li>
+	<li>contains a custom implementation of LISP (nujel)</li>
+	<li>lets you use a grappling hook to swing around those proceduraly generated islands</li>
+	<li>fits on a standard 1.44MB Floppy Disk</li>
+	<li>multiplayer with up to 32 players (might break before that, only tested up to 5)</li>
+	<li>has a nice fire simulation system that lets you unleash your inner pyromaniac</li>
+	<li>can run in your browser, even the one on your phone!</li>
+	</ul>
+	<hr/>
+	<p>A lot is still planned for this game, but since those are still in not complete or guaranteed to be in later versions of the game I do not wanna spoiler those that much, you can look into the various textfiles in the repo though to see some ideas. Since this game is developed without a fixed plan let me tell you about some priorities I have for this game:</p>
 	<h4>Cooperation over Competition</h4>
 	<p>Playing together as a group in a survival sandbox and fighting a common enemy have been some of the most enjoyable experiences I had in gaming, that is why I want to put most of my time into optimizing for this style instead of players competing with each other (apart from that there are already a lot of competitive shooters out there).</p>
 	<h4>Exploration over Grinding</h4>
@@ -84,7 +103,7 @@ foreach(array_keys($platforms) as $plat){
 	<h4>World-centric instead of Player-centric</h4>
 	<p>Most games focus solely on the players actions where pretty much everything needs to be initiated by the player, here I want to try out to put the world in the center, where things might just happen without anybody noticing or having something to do with it. One thing I would love to reach would be for it to be an enjoyable experience to just sit in-game and watch the scenery and events unfold.</p>
 	<h4>Performance over Bloat</h4>
-	<p>Performance is quite important to me because there are a lot of people who only have cheap laptops/phones instead of big gaming desktops. This combined with the focus on cooperative multiplayer forces me to pay extra attention to Performance/Bloat.</p>
+	<p>Performance is quite important to me because there are a lot of people who only have cheap laptops/phones instead of big gaming desktops. This combined with the focus on cooperative multiplayer forces me to pay extra attention to Performance/Bloat so that everyone can join in, even if they don't have the money for a fancy gaming rig.</p>
 	<hr/>
 	<h2>Controls</h2>
 	<h4>Mouse / Keyboard</h4>
@@ -102,9 +121,10 @@ foreach(array_keys($platforms) as $plat){
 	<br/>
 	<p><span class=key>N</span> to start flying around in no-clip mode.</p>
 	<p><span class=key>M</span> to toggle the debug overlayo.</p>
-	<p><span class=key>F2</span> to open/close your inventorye.</p>
-	<p><span class=key>F3</span> to open/close the crafting panel.</p>
+	<p><span class=key>F1</span> to switch to First-Person view.</p>
+	<p><span class=key>F3</span> to switch to Third-Person view.</p>
 	<p><span class=key>F11</span> to toggle Fullscreen mode.</p>
+	<p><span class=key>`</span><span class=key>F12</span> toggle LISP REPL.</p>
 	<br/>
 	<h4>Gamepad</h4>
 	<img src="<?php echo baseSF('img/padcontrols.png'); ?>" width=900 height=504 class="imgfw" />
