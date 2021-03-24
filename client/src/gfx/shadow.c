f23c4f988b401db67e7466ae0276e59656b8e652
Ben (RPI4) <ben@wolkenwelten.net>
2021-03-24 19:22:09 +0100

    Less shadow shakyness by also making use of the matSubBlockView Matrix

--- client/src/gfx/shadow.c
+++ client/src/gfx/shadow.c
@@ -40,8 +40,7 @@ void shadowDraw(){
 	glDepthFunc      (GL_LEQUAL);
 	meshFinishDynamic(meshShadow);
 	shaderBind       (sShadow);
-	matMov           (matMVP,matView);
-	matMul           (matMVP,matMVP,matProjection);
+	matMul           (matMVP,matSubBlockView,matProjection);
 	shaderMatrix     (sShadow,matMVP);
 
 	glPolygonOffset(-16,-16);
@@ -57,15 +56,16 @@ void shadowDraw(){
 void shadowAdd(const vec pos, float size){
 	vec p = pos;
 	p.y = floorf(p.y);
-	for(int i=0;i<12;i++){
+	for(int i=0;i<8;i++){
 		if(worldGetB(p.x,p.y-1.f,p.z) != 0){break;}
 		p.y -= 1.f;
-		if(i == 7){return;}
+		if(i >= 7){return;}
 	}
 	const float s = (1.2f + ((pos.y - p.y)/4.f))*size;
 	const float a = MAX(0.f,1.f-((pos.y - p.y)/11.f));
 	p.x -= s/2.f;
 	p.z -= s/2.f;
+	p = vecSub(p,subBlockViewOffset);
 
 	meshAddVertC(meshShadow, p.x  ,p.y,p.z  ,0.f,0.f,a);
 	meshAddVertC(meshShadow, p.x+s,p.y,p.z+s,1.f,1.f,a);
