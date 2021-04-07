5c32308946850b7f0a60cb717100755d7956b8a0
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-04-07 22:39:10 +0200

    Added temporary nujel-standalone assets to .gitignore

--- nujel-standalone/tmp/assets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stddef.h>
-
-extern        size_t saolib_nuj_len;
-extern unsigned char saolib_nuj_data[];
-
-extern        size_t stdlib_nuj_len;
-extern unsigned char stdlib_nuj_data[];
-
