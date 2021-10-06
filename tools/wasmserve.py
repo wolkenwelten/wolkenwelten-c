b039402c3d087f6b624310aa4122cc72c3d807a8
Ben (X13/Arch) <ben@wolkenwelten.net>
2021-10-06 14:58:15 +0200

    Made the wasmserver.py script much more convenient
    It now builds a new wasm release before starting the web server.
    Then it opens up a browser for the new local build, and quits the
    server when the tab/window gets closed.

--- tools/wasmserve.py
+++ tools/wasmserve.py
@@ -1,28 +1,55 @@
 #!/usr/bin/env python3
 import http.server
+import socket
 import socketserver
 import os
+import pathlib
+import threading
+import time
+import webbrowser
+from subprocess import call
 
-PORT = 8000
+PORT = 4200
 
-os.chdir("releases/wasm")
+class WolkenWeltenHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        '''Reads post request body'''
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes("Done", "utf-8"))
+        httpd.server_close()
+        quit()
 
-Handler = http.server.SimpleHTTPRequestHandler
+def open_browser():
+    time.sleep(0.01)
+    url = "http://localhost:" + str(PORT) + "/?savegame=test&debuginfo=1"
+    webbrowser.open_new(url)
+
+os.chdir(pathlib.Path(__file__).parent.resolve())
+call("./buildwasm")
+os.chdir("../releases/wasm")
+
+Handler = WolkenWeltenHandler
 Handler.extensions_map={
 	'.manifest': 'text/cache-manifest',
-	'.html': 'text/html',
-	'.png': 'image/png',
-	'.jpg': 'image/jpg',
-	'.svg':	'image/svg+xml',
-	'.css':	'text/css',
-	'.js':	'application/x-javascript',
-	'.wasm':	'application/wasm',
-	'': 'application/octet-stream', # Default
-}
+	'.html':     'text/html',
+	'.png':      'image/png',
+	'.jpg':      'image/jpg',
+	'.svg':      'image/svg+xml',
+	'.css':      'text/css',
+	'.js':       'application/x-javascript',
+	'.wasm':     'application/wasm',
+	'':          'application/octet-stream'}
+
 
 socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-
-print("serving at port", PORT)
-httpd.serve_forever()
+print("serving at port http://localhost:" + str(PORT))
+threading.Thread(target=open_browser).start()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    httpd.server_close()
