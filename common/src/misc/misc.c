9738d4aaa0f880009bc85e0547fe9e01ce6c863e
Ben (X13/Arch) <ben@wolkenwelten.net>
2021-11-14 00:30:42 +0100

    Refactored ansi escape sequence parser

--- common/src/misc/misc.c
+++ common/src/misc/misc.c
@@ -269,29 +269,66 @@ void changeToDataDir(){
 	}
 }
 
-int parseAnsiCode(const char *str, int *fgc, int *bgc){
-	int off = 0;
-	for(int i=0;i<7;i++){
-		if(str[i] == 0){return i;}
-	}
-	if(str[1] != '['){return 1;}
-	if(str[3] != ';'){
-		if(str[2] != '0'){return 1;}
-		if(str[3] != 'm'){return 1;}
+void parseAnsiCommand(int command, int *fgc, int *bgc){
+	switch(command){
+	case 0:
 		*fgc = 15;
-		return 4;
+		*bgc = 0;
+		break;
+	case 1:
+		*fgc |= 8;
+		break;
+	case 11:
+		*fgc &= 7;
+		break;
+	case 30:
+	case 31:
+	case 32:
+	case 33:
+	case 34:
+	case 35:
+	case 36:
+	case 37:
+		*fgc = (*fgc & 0x8) | ((command-30) & 0x7);
+		break;
+	case 39:
+		*fgc = (*fgc & 0x8) | 7;
+		break;
+
+	case 40:
+	case 41:
+	case 42:
+	case 43:
+	case 44:
+	case 45:
+	case 46:
+	case 47:
+		*bgc = (*bgc & 0x8) | ((command-40) & 0x7);
+		break;
+	case 49:
+		*bgc = (*bgc & 0x8) | 7;
+		break;
 	}
-	if(str[6] != 'm'){return 1;}
-	if(str[2] == '1'){
-		off = 8;
-	}else if(str[2] != '0'){
-		return 1;
+}
+
+int parseAnsiCode(const char *start, int *fgc, int *bgc){
+	if(start[1] != '['){return 1;}
+	const char *cur = &start[2];
+	const char *end = cur;
+	while(*end != 'm'){
+		for(;*end && isdigit((u8)*end);end++){}
+		if(*end == 0){break;}
+		if((*end == 'm') || (*end == ';')){
+			const int command = atoi(cur);
+			parseAnsiCommand(command,fgc,bgc);
+			if(*end++ == 'm'){break;}
+			cur = end;
+			continue;
+		}
+		end++;
 	}
-	if(str[4] != '3'){return 1;}
-	if((str[5] < '0') || (str[5] > '8')){return 1;}
-	*fgc = off + str[5] - '0';
-	*bgc = -1;
-	return 7;
+	const int ret = end - start;
+	return ret;
 }
 
 u64 SHA1Simple(const void *data, uint len){
