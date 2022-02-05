c3469fbbbaf36a21fd9fdbe7fba0aac6cf02d8cb
Ben (RPI400) <ben@wolkenwelten.net>
2022-02-05 17:50:13 +0100

    Fixed issue with 32-bit systems

--- common/src/game/light.c
+++ common/src/game/light.c
@@ -34,114 +34,111 @@ static int lightOff(int x, int y, int z){
 }
 
 static void lightBlurZ(u8 *out){
-        PROFILE_START();
+	PROFILE_START();
 	for(int x=0;x < LIGHT_SIZE;x++){
 	for(int y=0;y < LIGHT_SIZE;y++){
 	i8 a = 0;
-        i8 b = 0;
+	i8 b = 0;
 	for(int z=0;z < LIGHT_SIZE;z++){
-                int off = lightOff(x,y,z);
+		int off = lightOff(x,y,z);
 		a = out[off] = MAX(out[off], a);
 		a = MAX(a - 2, 0);
 
-                int bff = lightOff(x,y,(LIGHT_SIZE-1)-z);
-                b = out[bff] = MAX(out[bff], b);
+		int bff = lightOff(x,y,(LIGHT_SIZE-1)-z);
+		b = out[bff] = MAX(out[bff], b);
 		b = MAX(b - 2, 0);
 	}
 	}
 	}
-        PROFILE_STOP();
+	PROFILE_STOP();
 }
 
 static void lightBlurY(u8 *out){
-        PROFILE_START();
+	PROFILE_START();
 	for(int x=0;x < LIGHT_SIZE;x++){
 	for(int z=0;z < LIGHT_SIZE;z++){
 	i8 a = 0;
-        i8 b = 0;
+	i8 b = 0;
 	for(int y=0;y < LIGHT_SIZE;y++){
 		const int off = lightOff(x,y,z);
 		a = out[off] = MAX(out[off], a);
 		a = MAX(a - 2, 0);
 
-                const int bff = lightOff(x,(LIGHT_SIZE-1) - y,z);
+		const int bff = lightOff(x,(LIGHT_SIZE-1) - y,z);
 		b = out[bff] = MAX(out[bff], b);
 		b = MAX(b - 2, 0);
 	}
 	}
 	}
-        PROFILE_STOP();
+	PROFILE_STOP();
 }
 
 static void lightBlurX(u8 *out){
-        PROFILE_START();
+	PROFILE_START();
 	for(int y=0;y < LIGHT_SIZE;y++){
 	for(int z=0;z < LIGHT_SIZE;z++){
 	i8 a = 0;
-        i8 b = 0;
+	i8 b = 0;
 	for(int x=0;x < LIGHT_SIZE;x++){
 		const int off = lightOff(x,y,z);
 		a = out[off] = MAX(out[off], a);
 		a = MAX(a - 2, 0);
 
-                const int bff = lightOff((LIGHT_SIZE-1) - x,y,z);
+		const int bff = lightOff((LIGHT_SIZE-1) - x,y,z);
 		b = out[bff] = MAX(out[bff], b);
 		b = MAX(b - 2, 0);
 	}
 	}
 	}
-        PROFILE_STOP();
+	PROFILE_STOP();
 }
 
 static void lightBlur(u8 *buf){
-        PROFILE_START();
-        lightBlurZ(buf);
+	PROFILE_START();
+	lightBlurZ(buf);
 	lightBlurX(buf);
 	lightBlurY(buf);
-        PROFILE_STOP();
+	PROFILE_STOP();
 }
 
 static void lightSunlight(u8 *out,const chunkOverlay *block[3][3][3]){
-        PROFILE_START();
+	PROFILE_START();
 	const u8 sunlight = gtimeGetBlockBrightness(gtimeGetTimeOfDay());
 	for(int x=0;x<LIGHT_SIZE;x++){
-                const u8 cx = x & 0xF;
+		const u8 cx = x & 0xF;
 	for(int z=0;z<LIGHT_SIZE;z++){
-                const u8 cz = z & 0xF;
-                u8 curLight = sunlight;
+		const u8 cz = z & 0xF;
+		u8 curLight = sunlight;
 	for(int cy=2;cy>=0;cy--){
 		const chunkOverlay *cur = block[x/CHUNK_SIZE][cy][z/CHUNK_SIZE];
-                const int ccy = cy * CHUNK_SIZE;
-                if(cur){
-                        for(int y=CHUNK_SIZE-1;y>=0;y--){
-                                const int off = lightOff(x,y+ccy,z);
-                                const blockId b = cur->data[cx][y][cz];
-                                if(b){
-                                        out[off] = blockTypeGetLightEmission(b);
-                                        curLight = 0;
-                                }else{
-                                        out[off] = curLight = MIN(sunlight, curLight+2);
-                                }
-                        }
-                }else{
-                        for(int y=CHUNK_SIZE-1;y>=0;y--){
-                                const int off = lightOff(x,y+ccy,z);
-                                out[off] = curLight = MIN(sunlight, curLight+2);
-                        }
-                }
+		const int ccy = cy * CHUNK_SIZE;
+		if(cur){
+			for(int y=CHUNK_SIZE-1;y>=0;y--){
+				const int off = lightOff(x,y+ccy,z);
+				const blockId b = cur->data[cx][y][cz];
+				if(b){
+					out[off] = blockTypeGetLightEmission(b);
+					curLight = 0;
+				}else{
+					out[off] = curLight = MIN(sunlight, curLight+2);
+				}
+			}
+		}else{
+			for(int y=CHUNK_SIZE-1;y>=0;y--){
+				const int off = lightOff(x,y+ccy,z);
+				out[off] = curLight = MIN(sunlight, curLight+2);
+			}
+		}
 	}
 	}
 	}
-        PROFILE_STOP();
+	PROFILE_STOP();
 }
 
 static void lightOut(u8 *in, chunkOverlay *out){
-        u64 *o = (u64 *)&out->data[0][0][0];
 	for(int x=CHUNK_SIZE;x<CHUNK_SIZE*2;x++){
 	for(int y=CHUNK_SIZE;y<CHUNK_SIZE*2;y++){
-                const u64 *i = (const u64 *)&in[lightOff(x,y,CHUNK_SIZE)];
-		*o++ = *i++;
-		*o++ = *i++;
+		memcpy(&out->data[x-CHUNK_SIZE][y-CHUNK_SIZE][0],&in[lightOff(x,y,CHUNK_SIZE)],CHUNK_SIZE);
 	}
 	}
 }
@@ -151,6 +148,6 @@ void lightTick(chunkOverlay *light, const chunkOverlay *block[3][3][3]){
 	PROFILE_START();
 	lightSunlight(lightBuffer, block);
 	lightBlur(lightBuffer);
-        lightOut(lightBuffer,light);
+	lightOut(lightBuffer,light);
 	PROFILE_STOP();
 }
