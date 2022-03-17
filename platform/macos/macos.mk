c09bb04fdea432b0c38d100574560e63e5e99e31
Ben (Mac) <ben@wolkenwelten.net>
2022-03-17 21:51:04 +0000

    Fixed MacOS releases

--- platform/macos/macos.mk
+++ platform/macos/macos.mk
@@ -25,31 +25,18 @@ releases/macos/wolkenwelten-macos-$(VERSION_NAME).tgz: $(OSX_APP)/Contents/Frame
 releases/macos/wolkenwelten-macos-$(VERSION_NAME).tgz: $(OSX_APP)/Contents/Resources/wolkenwelten.icns
 	cd releases/macos/ && tar -czf wolkenwelten-macos-$(VERSION_NAME).tgz wolkenwelten-$(VERSION_NAME).app
 
-$(OSX_APP)/Contents/MacOS/wolkenwelten: CFLAGS    += $(CLIENT_CFLAGS)
-$(OSX_APP)/Contents/MacOS/wolkenwelten: CINCLUDES += $(CLIENT_CINCLUDES)
-$(OSX_APP)/Contents/MacOS/wolkenwelten: LIBS      += $(CLIENT_LIBS)
-$(OSX_APP)/Contents/MacOS/wolkenwelten: $(CLIENT_SRCS) $(CLIENT_HDRS)
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/gfxAssets.c client/src/tmp/gfxAssets.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/sfxAssets.c client/src/tmp/sfxAssets.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/shdAssets.c client/src/tmp/shdAssets.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/txtAssets.c client/src/tmp/txtAssets.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/nujAssets.c client/src/tmp/nujAssets.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/meshAssets.c client/src/tmp/meshAssets.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/objs.c client/src/tmp/objs.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: client/src/tmp/sfx.c client/src/tmp/sfx.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten: common/src/tmp/cto.c
+$(OSX_APP)/Contents/MacOS/wolkenwelten: $(CLIENT_SRCS) $(CLIENT_TMP_SRCS)
+$(OSX_APP)/Contents/MacOS/wolkenwelten: $(ASM_OBJS) common/nujel/nujel.a common/nujel/tmp/stdlib.o
 	mkdir -p $(OSX_APP)/Contents/MacOS
-	$(CC) $(CLIENT_SRCS) -o $@ $(RELEASE_OPTIMIZATION) $(CFLAGS) $(CINCLUDES) $(WARNINGS) $(LIBS)
+	$(CC) $^ -o $@ $(RELEASE_OPTIMIZATION) $(CLIENT_CFLAGS) $(CFLAGS) $(CLIENT_CINCLUDES) $(CINCLUDES) $(WARNINGS) $(CLIENT_LIBS) $(LIBS)
 	strip -SxX $@
 	install_name_tool -change "@rpath/SDL2.framework/Versions/A/SDL2" "@executable_path/../Frameworks/SDL2.framework/SDL2" $@
 	install_name_tool -change "@rpath/SDL2_mixer.framework/Versions/A/SDL2_mixer" "@executable_path/../Frameworks/SDL2_mixer.framework/SDL2_mixer" $@
 
-$(OSX_APP)/Contents/MacOS/wolkenwelten-server: $(SERVER_SRCS) $(SERVER_HDRS)
-$(OSX_APP)/Contents/MacOS/wolkenwelten-server: server/src/tmp/sfx.c server/src/tmp/sfx.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten-server: server/src/tmp/objs.c server/src/tmp/objs.h
-$(OSX_APP)/Contents/MacOS/wolkenwelten-server: common/src/tmp/cto.c
+$(OSX_APP)/Contents/MacOS/wolkenwelten-server: $(SERVER_SRCS) $(SERVER_TMP_SRCS)
+$(OSX_APP)/Contents/MacOS/wolkenwelten-server: $(ASM_OBJS) common/nujel/nujel.a common/nujel/tmp/stdlib.o
 	mkdir -p $(OSX_APP)/Contents/MacOS
-	$(CC) $(SERVER_SRCS) -o $@ $(RELEASE_OPTIMIZATION) $(CFLAGS) $(CINCLUDES) $(WARNINGS) $(LIBS)
+	$(CC) $^ -o $@ $(RELEASE_OPTIMIZATION) $(CFLAGS) $(SERVER_CFLAGS) $(CSTD) $(CINCLUDES) $(SERVER_CINCLUDES) $(LIBS) $(SERVER_LIBS)
 	strip -SxX $@
 
 $(OSX_APP)/Contents/Info.plist:  platform/macos/Info.plist
@@ -76,30 +63,30 @@ $(OSX_APP)/Contents/Resources/wolkenwelten.icns: releases/macos/wolkenwelten.ico
 	mkdir -p $(OSX_APP)/Contents/Resources/
 	iconutil -c icns -o $@ releases/macos/wolkenwelten.iconset
 
-releases/macos/wolkenwelten.iconset/icon_16x16.png: web/favicon16.png
+releases/macos/wolkenwelten.iconset/icon_16x16.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "16x16" $< $@
-releases/macos/wolkenwelten.iconset/icon_32x32.png: web/favicon32.png
+releases/macos/wolkenwelten.iconset/icon_32x32.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "32x32" $< $@
-releases/macos/wolkenwelten.iconset/icon_16x16@2x.png: web/favicon32.png
+releases/macos/wolkenwelten.iconset/icon_16x16@2x.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "32x32" $< $@
-releases/macos/wolkenwelten.iconset/icon_32x32@2x.png: web/favicon.png
+releases/macos/wolkenwelten.iconset/icon_32x32@2x.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "64x64" $< $@
-releases/macos/wolkenwelten.iconset/icon_64x64.png: web/favicon.png
+releases/macos/wolkenwelten.iconset/icon_64x64.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "64x64" $< $@
-releases/macos/wolkenwelten.iconset/icon_64x64@2x.png: web/favicon.png
+releases/macos/wolkenwelten.iconset/icon_64x64@2x.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "128x128" $< $@
-releases/macos/wolkenwelten.iconset/icon_128x128.png: web/favicon.png
+releases/macos/wolkenwelten.iconset/icon_128x128.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "128x128" $< $@
-releases/macos/wolkenwelten.iconset/icon_128x128@2x.png: web/favicon.png
+releases/macos/wolkenwelten.iconset/icon_128x128@2x.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "256x256" $< $@
-releases/macos/wolkenwelten.iconset/icon_256x256.png: web/favicon.png
+releases/macos/wolkenwelten.iconset/icon_256x256.png: logo.png
 	mkdir -p releases/macos/wolkenwelten.iconset/
 	convert -resize "256x256" $< $@
